95b1c68e3b7977e7275ea3926c5cbba7eada3ad4
Bradley Buda <bradleybuda@gmail.com>
2021-04-07 17:01:12 -0700

    Minimal implemenation + readme

---
+++ samples/minimal/README.md
@@ -0,0 +1,6 @@
+This is a pure javascript, zero-dependencies minimal example of a Custom API
+implementation that does nothing, but exercises most of the features of the
+protocol. It's useful for learning the Custom API protocol at a low level, or
+for rapidly deploying without requiring a build process or dependency
+management, but the other examples may be better jumping-off points for
+developing a maintainable Custom API implementation.
---
+++ samples/minimal/index.js
@@ -0,0 +1,95 @@
+// Mimimal conforming custom connection implementation - zero external
+// dependencies
+
+const server = {};
+
+server.test_connection = () => {
+  return { success: true };
+};
+
+server.list_objects = () => {
+  return {
+    objects: [{ object_api_name: "customer", label: "Customers" }],
+  };
+};
+
+server.supported_operations = ({ object }) => {
+  console.log("listing operations for object", object);
+  return { operations: ["upsert"] };
+};
+
+server.list_fields = ({ object }) => {
+  console.log("listing fields for object", object);
+  return {
+    fields: [
+      {
+        field_api_name: "email",
+        label: "Email",
+        identifier: true,
+        createable: true,
+        updateable: true,
+        type: "string",
+        required: true,
+        array: false,
+      },
+      {
+        field_api_name: "name",
+        label: "Name",
+        identifier: false,
+        createable: true,
+        updateable: true,
+        type: "string",
+        required: true,
+        array: false,
+      },
+    ],
+  };
+};
+
+server.get_sync_speed = ({ sync_plan }) => {
+  console.log("get sync speed for sync", sync_plan);
+  return {
+    maximum_batch_size: 2,
+    maximum_records_per_second: 1,
+    maximum_parallel_batches: 1,
+  };
+};
+
+server.sync_batch = ({ sync_plan, records }) => {
+  console.log("sync one batch of data", { sync_plan, records });
+  return {
+    record_results: records.map((record, index) => {
+      success = [true, false][index % 2];
+      return {
+        identifier: record.email,
+        success,
+        error_message: success ? null : "oops!",
+      };
+    }),
+  };
+};
+
+const port = 6161;
+
+require("http")
+  .createServer((req, res) => {
+    var requestBodyBuffer = "";
+    req.on("data", (chunk) => (requestBodyBuffer += chunk));
+    req.on("end", () => {
+      //console.log({requestBody})
+      const { id, method, params } = JSON.parse(requestBodyBuffer);
+
+      const result = server[method](params);
+
+      const body = {
+        jsonrpc: "2.0",
+        id,
+        result,
+      };
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(body));
+      res.end();
+    });
+  })
+  .listen(port);
